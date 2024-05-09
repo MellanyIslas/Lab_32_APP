f2dce89a9b682c9c32c62b745d071c4c747fbbd5
MellanyIslas <mellanynislas@gmail.com>
2024-05-08 20:21:39 -0600

    Practica 9
    Practica 9

---
+++ ionic-basic/src/app/destinos/destinos.page.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Lugar } from '../interface/lugar';
+import { AuthFirebaseService } from '../service/auth-firebase.service';
+
+@Component({
+  selector: 'app-destinos',
+  templateUrl: './destinos.page.html'
+})
+export class DestinosPage implements OnInit {
+
+  lugar: Lugar = new Lugar();
+  destinos: any[] = [];
+  ionicForm: any;
+
+  constructor(
+    private authService: AuthFirebaseService,
+    private formBuilder: FormBuilder
+  ) { 
+    
+  }
+  ngOnInit() {
+    this.buildForm();
+    this.authService.getLugares(this.destinos);
+  }
+
+  buildForm(){
+    this.ionicForm = this.formBuilder.group({
+      nombre: new FormControl('',{validators: [Validators.required]})
+    });
+  }  
+
+  ionViewWillEnter(){
+    this.authService.getLugares(this.destinos);
+  }
+
+  altaLugar(){
+    this.authService.altaLugar(this.lugar);
+    this.authService.getLugares(this.destinos);
+    this.ionicForm.reset();
+  }
+
+  submitForm(){
+    if(this.ionicForm.valid){
+      this.lugar.nombre = this.ionicForm.get('nombre').value;
+      this.altaLugar();
+    }
+  }
+
+  hasError: any = (controlName: string, errorName: string) => {
+    return !this.ionicForm.controls[controlName].valid &&
+      this.ionicForm.controls[controlName].hasError(errorName) &&
+      this.ionicForm.controls[controlName].touched;
+  }  
+}
